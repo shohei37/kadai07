d8c4075cdbcfae55f178717979c87a6ce727bc43
shohei37 <102854135+shohei37@users.noreply.github.com>
2022-06-02 18:23:31 +0900

    Add files via upload

---
+++ gs_an_table.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: localhost:8889
+-- 生成日時: 2022 年 6 月 02 日 09:23
+-- サーバのバージョン： 5.7.34
+-- PHP のバージョン: 7.4.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `gs_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `gs_an_table`
+--
+
+CREATE TABLE `gs_an_table` (
+  `id` int(12) NOT NULL,
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `birthday` date NOT NULL,
+  `living` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `foodtype` varchar(64) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `gs_an_table`
+--
+
+INSERT INTO `gs_an_table` (`id`, `name`, `email`, `birthday`, `living`, `foodtype`) VALUES
+(35, '奥田', '翔平', '2022-06-09', '東京', 'ケトジェニック'),
+(36, '奥田', 'syttpp@gmail.com', '2022-06-09', '東京', 'ケトジェニック'),
+(37, '奥田', 'syttpp@gmail.com', '2022-06-15', '', 'ケトジェニック');
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `gs_an_table`
+--
+ALTER TABLE `gs_an_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `gs_an_table`
+--
+ALTER TABLE `gs_an_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
